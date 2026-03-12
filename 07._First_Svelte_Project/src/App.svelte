4b45c30c2bdf5b50622fba74b3b7aa9b81ea8743
Gustav Jacobsen <gustavtorup@gmail.com>
2026-03-12 11:47:34 +0100

    information fra basefil bliver nu udfyldt i pcschematic

---
+++ 07._First_Svelte_Project/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Counter from "./components/Counter/Counter.svelte";
+
+</script>
+<h1>Header.... welcome to the first svelte project</h1>
+
+<h2>What do you want to see</h2>
+
+<Counter countdownFrom={10}></Counter>
+
+<Counter countdownFrom={100}></Counter>
---
+++ 07._First_Svelte_Project/src/components/Counter/Counter.svelte
@@ -0,0 +1,17 @@
+
+<script>
+    const { countdownFrom } = $props();
+
+    let count = $state(countdownFrom);
+
+    function decreateCount(){
+        count -= 1;
+    }
+</script>
+
+
+<h3>Counter </h3>
+<h4>{count}</h4>
+
+
+<button onclick={decreateCount}>Decrease</button>
